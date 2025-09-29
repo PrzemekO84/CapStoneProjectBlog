f088f94abb4afdc2928506e3d23a6a9dab786f4a
PrzemekO84 <przemekorzechowski53@gmail.com>
2025-09-29 21:08:14 +0200

    Worked on year selector

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import fs from "fs";
+import { releaseDates } from "./logic/appLogic.js";
 
 const port = 3000;
 const app = express();
+
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -10,11 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 const posts = JSON.parse(fs.readFileSync("gamesPosts.json", "utf8"));
 
 app.get("/", (req, res) => {
+    const gameReleaseDates = releaseDates();
     res.render("index.ejs",
         {
-            posts: posts
+            posts: posts,
+            releaseDates: gameReleaseDates
         }
     );
+    console.log("XDDD");
+    console.log(gameReleaseDates);
 });
 
 app.get("/about", (req, res) => {
---
+++ logic/appLogic.js
@@ -0,0 +1,16 @@
+
+export function releaseDates(){
+
+    const currentYear = new Date().getFullYear();
+    let startDate = 1958;
+
+    const yearList = [];
+
+    for (let i = startDate; i <= currentYear; i++) {
+        yearList.push(startDate);
+        startDate++;
+    };
+
+    return yearList;
+};
+
